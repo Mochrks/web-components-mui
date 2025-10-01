2eb7e98211ccfeaaf1239ed7512c0b1d63a32dec
MOCH. RIZKI KURNIAWAN <92960397+Mochrks@users.noreply.github.com>
2025-10-01 09:58:53 +0700

    fix: message commit lint config

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,7 +3,5 @@ module.exports = {
   rules: {
     'type-case': [2, 'always', 'lower-case'],
     'subject-case': [2, 'always', 'lower-case'],
-    'body-case': [2, 'always', 'lower-case'],
-    'footer-case': [2, 'always', 'lower-case']
   }
 }
